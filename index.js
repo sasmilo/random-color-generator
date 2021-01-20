1d072aad6bd2760f149bf320914a9f913fdce83b
Sasa Milosevic <sasmilo20@gmail.com>
2021-01-20 01:35:00 +0100

    Correction of values

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const randomColor = require('randomcolor'); // import the script
 const chalk = require('chalk'); // import chalk
-const log = console.log;
 
 if (process.argv.length > 4) {
   const color = randomColor({
     luminosity: process.argv[3],
     hue: process.argv[2],
   });
+
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('#####                      #####'));
+  console.log(chalk.hex(color)('#####       ' + color + '        #####'));
+  console.log(chalk.hex(color)('#####                      #####'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
 }
 if (process.argv.length === 4) {
   const color = randomColor({
@@ -14,6 +24,16 @@ if (process.argv.length === 4) {
     hue: process.argv[2],
   }); // Returns a hex code for a color represented with parameters of luminosity and hue
   console.log(color); // Logs the colors' hex code to console
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('#####                      #####'));
+  console.log(chalk.hex(color)('#####       ' + color + '        #####'));
+  console.log(chalk.hex(color)('#####                      #####'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
 }
 
 if (process.argv.length === 3) {
@@ -21,22 +41,42 @@ if (process.argv.length === 3) {
     hue: process.argv[2],
   }); // Returns a hex code for a color represented with parameter of  hue
   console.log(color); // Logs the colors' hex code to console
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('#####                      #####'));
+  console.log(chalk.hex(color)('#####       ' + color + '        #####'));
+  console.log(chalk.hex(color)('#####                      #####'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
 }
 
 if (process.argv.length === 2) {
   const color = randomColor(); // Returns a hex code for a random color
   console.log(color); // Logs the colors' hex code to console
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('#####                      #####'));
+  console.log(chalk.hex(color)('#####       ' + color + '        #####'));
+  console.log(chalk.hex(color)('#####                      #####'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
+  console.log(chalk.hex(color)('################################'));
 }
 
-console.log(chalk.hex(color)('################################'));
-console.log(chalk.hex(color)('################################'));
-console.log(chalk.hex(color)('################################'));
-console.log(chalk.hex(color)('################################'));
-console.log(chalk.hex(color)('#####                      #####'));
-console.log(chalk.hex(color)('#####       ' + color + '        #####'));
-console.log(chalk.hex(color)('#####                      #####'));
-console.log(chalk.hex(color)('################################'));
-console.log(chalk.hex(color)('################################'));
-console.log(chalk.hex(color)('################################'));
+//console.log(chalk.hex(color)('################################'));
+//console.log(chalk.hex(color)('################################'));
+//console.log(chalk.hex(color)('################################'));
+//console.log(chalk.hex(color)('################################'));
+//console.log(chalk.hex(color)('#####                      #####'));
+//console.log(chalk.hex(color)('#####       ' + color + '        #####'));
+//console.log(chalk.hex(color)('#####                      #####'));
+//console.log(chalk.hex(color)('################################'));
+//console.log(chalk.hex(color)('################################'));
+//console.log(chalk.hex(color)('################################'));
 
 //console.log(chalk.blue('Hello world!'));
